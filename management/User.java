6cad2d267ce0a506cc26ae3f3609111dc21f8543
chandrashekhar mahagonkar <shekarmaharaj564@gmail.com>
2026-05-31 13:42:05 +0530

    Add files via upload

---
+++ management/User.java
@@ -0,0 +1,26 @@
+package com.library.management;
+
+public class User {
+
+    private int userId;
+    private String name;
+
+    public User(int userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "User ID: " + userId +
+                " | Name: " + name;
+    }
+}
